2ad4576b15934f90dc9a570191119ee25e357237
Thomas Uebel <t.uebel@gmail.com>
2026-04-03 14:56:20 +0200

    test: red phase for Phase 7 bStats integration

---
+++ src/test/java/de/thomasuebel/lastactiveplayers/stats/NoStatisticsTest.java
@@ -0,0 +1,11 @@
+package de.thomasuebel.lastactiveplayers.stats;
+
+import org.junit.jupiter.api.Test;
+
+class NoStatisticsTest {
+
+    @Test
+    void canBeConstructed() {
+        new NoStatistics();
+    }
+}
